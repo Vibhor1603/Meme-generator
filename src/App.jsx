d84c0e85aea19ce42af61032bcc9cc4085f56864
Vibhor Sharma <vibhorsharmak@gmail.com>
2024-08-02 00:29:28 +0530

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react"
 import Header from "./Header.jsx"
-import Meme from "././Meme.jsx"
+import Meme from "./Meme.jsx"
 
 /**
  * Challenge: Build the Header component
